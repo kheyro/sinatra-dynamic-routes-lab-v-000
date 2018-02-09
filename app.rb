d57cfc3f2e175cbb9a5caa73d22ea64196e38dae
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-09 13:59:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,22 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @revername = params[:name].reverse
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    @square = params[:number].to_i
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    erb :say
+  end
+
+  
+    
 end
